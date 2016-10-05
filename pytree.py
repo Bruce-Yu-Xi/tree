e667081d76ed09a50c4a3fb2c60af7f3bdf080a9
Bruce-Yu-Xi <xy333@cornell.edu>
2016-10-05 02:16:19 -0400

    Update pytree.py

--- pytree.py
+++ pytree.py
@@ -7,31 +7,35 @@ indent = '│   '
 indent_done = '    '
 child_branch = '├── '
 child_branch_done = '└── '
-file_count = 0
-dir_count = 0
+fcount = 0
+pathcount = 0
 
 
 def tree_generate(path, symbol):
-    global dir_count
-    global file_count
-    all_children = sorted([c for c in os.listdir(path) if not c.startswith('.')], key=lambda s: s.strip('_').lower())
-    end_file = 0
-    num_children = 0
-    for children in all_children:
-        if(num_children < (len(all_children) - 1)):
+    global dircount
+    global filecount
+    alld = []
+    for i in os.listdir(path):
+        if not i.startswith('.'):
+            alld += [i]
+    alld.sort(key=lambda s: s.strip('_').lower())
+    end = False
+    num = 0
+    for children in alld:
+        if(num < (len(alld) - 1)):
             print(symbol + child_branch + str(children))
         else:
-            end_file = 1
+            end = True
             print(symbol + child_branch_done + str(children))
         if (os.path.isdir(os.path.join(path, children))):
-            dir_count = dir_count + 1
-            if(end_file == 1):
+            pathcount += 1
+            if end:
                 tree_generate(os.path.join(path, children), symbol + indent_done)
             else:
                 tree_generate(os.path.join(path, children), symbol + indent)
         elif(os.path.isfile(os.path.join(path, children))):
-            file_count = file_count + 1
-        num_children = num_children + 1
+            fcount += 1
+        num += 1
 
 if __name__ == '__main__':
     dir_path = "."
@@ -40,4 +44,4 @@ if __name__ == '__main__':
     print(dir_path)
     tree_generate(dir_path, "")
     print()
-    print(str(dir_count) + " directories, " + str(file_count) + " files")
+    print(str(pathcount) + " directories, " + str(fcount) + " files")
