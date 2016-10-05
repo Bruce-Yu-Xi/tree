f17ad80bc3e1263c9349236bb025d18751679857
Bruce-Yu-Xi <xy333@cornell.edu>
2016-10-05 01:37:43 -0400

    Update pytree.py

--- pytree.py
+++ pytree.py
@@ -1,62 +1,41 @@
-#!/usr/bin/env python3
 import subprocess
 import sys
 import os
-import string
-import re
 
 
-def printName(indent, i, lastElement):
-    if i == lastElement:
-        print(indent + '└── ' + str(i))
-        indent1 = indent + '    '
-    else:
-        print(indent + '├── ' + str(i))
-        indent1 = indent + '│   '
-    return indent1
+def display(directory):
+    for directory in directories:
+        if os.path.isdir(directory):
+            search_and_print_node(directory)
+        else:
+            print("Directory " + directory + " doesn't exist!")
+    return
 
 
-def sort_key(s):
-    return re.sub('[^A-Za-z]+', '', s).lower()
+def search_and_print_node(node, prefix=''):
+    items = os.listdir(node)
+    if len(items) == 0:
+        return
 
-
-def sortList(curList):
-    curList = [item for item in curList if item[0] != ('.')]
-    curList = sorted(curList, key=sort_key)
-    return curList
-
-
-def printDir(path, file_cnt, dir_cnt, indent=''):
-    if (path[-1] != '/'):
-        path = path + '/'
-    curList = os.listdir(path)
-    curList = sortList(curList)
-    cnt = []
-    for i in curList:
-        curPath = path + i
-        if (os.path.isdir(curPath)):
-            curPath = curPath + '/'
-            dir_cnt = dir_cnt + 1
-            indent1 = printName(indent, i, curList[-1])
-            cnt1 = printDir(curPath, file_cnt, dir_cnt, indent1)
-            file_cnt = cnt1[1]
-            dir_cnt = cnt1[0]
-        elif(os.path.isfile(curPath)):
-            printName(indent, i, curList[-1])
-            file_cnt = file_cnt + 1
-    cnt.append(dir_cnt)
-    cnt.append(file_cnt)
-    return cnt
-
+    items.sort()
+    key = 0
+    currentPrefix = "├──"
+    nextPrefix = "│  "
+    while key < len(items):
+        if key == (len(items) - 1):
+            currentPrefix = "└──"
+            nextPrefix = "   "
+        print(prefix + currentPrefix + items[key])
+        if os.path.isdir(os.path.join(node, items[key])):
+            search_and_print_node(os.path.join(node, items[key]), nextPrefix)
+        key += 1
+    return
 
 if __name__ == '__main__':
 
-    subprocess.run(['tree'] + sys.argv[1:])
-    if (len(sys.argv) == 1):
-        path = '.'
+    if len(sys.argv) > 1:
+        directories = sys.argv[1:]
     else:
-        path = sys.argv[1]
-    print(path)
-    cnt = printDir(path, 0, 0, '')
-    print()
-    print('%d directories, %d files' % (cnt[0], cnt[1]))
+        directories = ["."]
+
+    display(directories)
